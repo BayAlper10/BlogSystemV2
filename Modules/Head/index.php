227583ce9b99f6c9e5223f742d73af05a77796fc
BayAlper10 <dursunalper1@gmail.com>
2019-06-14 00:07:34 +0300

    localhost güncellemesi

--- Modules/Head/index.php
+++ Modules/Head/index.php
@@ -5,7 +5,7 @@ class Head{
   }
   public function getHead(){
     ?>
-    <base href="http://25.77.207.204/" />
+    <base href="http://localhost/" />
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
